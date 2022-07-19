371a92e0406003eb0d3e8764b8d44d9ee3695c36
NatalliaE <natasha.ehlis@gmail.com>
2022-07-19 13:42:16 +0300

    add ButtonProps

---
+++ src/components/ButtonProps/ButtonProps.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ButtonProps.css';
+
+const ButtonProps = () => {
+  return (
+    <button className='my-button_props'>Click me!!!</button>
+  )
+}
+
+export default ButtonProps;
